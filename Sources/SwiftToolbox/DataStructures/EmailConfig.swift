a1d13104f0b5c6caf355ccd194b23db93596e1b4
Ryan Krol <ryankr@hotmail.co.uk>
2018-12-22 02:14:52 +0000

    Add EmailConfig codable

---
+++ Sources/SwiftToolbox/DataStructures/EmailConfig.swift
@@ -0,0 +1,16 @@
+//
+//  EmailConfig.swift
+//  SwiftToolbox
+//
+//  Created by Ryan Krol on 22/12/2018.
+//
+//  TV: Peep Show = Season 3 - Episode 6
+
+import Foundation
+
+public struct EmailConfig: Decodable {
+    let emailAccount: String
+    let password: String
+    let hostName: String
+}
+
